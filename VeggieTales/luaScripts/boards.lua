b3a0231169b6b42e2a53a80833f348616eb7b616
Cegaiel <sharpguy32@yahoo.com>
2012-04-07 21:06:20 -0400

    Added a needed delay to make sure all benches get clicked correctly. Adjust the timer between clicks (can be configured by user at top of script). It now also shows how many windows its refreshing (pinned up) and how many windows it is clicking. If you see it is clicking less windows than its refreshing, then that suggests one of your carp blades needs replaced.

--- VeggieTales/luaScripts/boards.lua
+++ VeggieTales/luaScripts/boards.lua
@@ -7,6 +7,9 @@ loadfile("luaScripts/ui_utils.inc")();
 
 per_click_delay = 10;
 
+--pause time is how long it waits after clicking on 'Plane a piece of wood' button (1000 ms = 1 second)
+pause_time = 2500; 
+
 right_click = nil;
 
 function doit()
@@ -14,47 +17,51 @@ function doit()
 	while 1 do
 		local image_name = "ThisIs.png";
 
-		-- Find buttons and click them!
+		-- Click pin ups to refresh the window
 		srReadScreen();
 		xyWindowSize = srGetWindowSize();
 		local buttons = findAllImages(image_name);
 		
 		if #buttons == 0 then
-			error 'Could not find \'Start Planing\' button.'
-			--statusScreen("Could not find specified buttons...");
-			--lsSleep(1500);
+
+			error 'Could not find any pinned windows.'
+
 		else
-			statusScreen("Clicking " .. #buttons .. "button(s)...");
+			statusScreen("Refreshing (" .. #buttons .. " windows)");
 			for i=#buttons, 1, -1 do
 				srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3, right_click);
 				lsSleep(per_click_delay);
 			end
-			statusScreen("Done clicking (" .. #buttons .. " clicks).");
-			lsSleep(100);
+
 		end
+
+
+			lsSleep(200);
+
+
 
 		image_name = "PlaneAPiece.png";
 
 		-- Find buttons and click them!
 		srReadScreen();
 		xyWindowSize = srGetWindowSize();
-		local buttons = findAllImages(image_name);
+		local buttons2 = findAllImages(image_name);
 		
-		if #buttons == 0 then
-			error 'Could not find any windows.'
-			--statusScreen("Could not find specified buttons...");
-			--lsSleep(1500);
+		if #buttons2 == 0 then
+			statusScreen("Refreshing (" .. #buttons .. " windows)\n\nWaiting for buttons...");
+
 		else
-			statusScreen("Clicking " .. #buttons .. "button(s)...");
-			for i=#buttons, 1, -1 do
-				srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3, right_click);
+
+			for i=#buttons2, 1, -1 do
+				srClickMouseNoMove(buttons2[i][0]+5, buttons2[i][1]+3, right_click);
 				lsSleep(per_click_delay);
 			end
-			statusScreen("Done clicking (" .. #buttons .. " clicks).");
-			for j=1, 4 do
-				lsSleep(1000);
-				statusScreen("Waiting...");
-			end;
+
+
+			statusScreen("Done clicking (" .. #buttons2 .. " windows)\n\nPausing...");
+				lsSleep(pause_time);
+
+
 		end
 
 	end
