079e717091c14810b9256ecb6b383044cd0270ff
Cegaiel <sharpguy32@yahoo.com>
2014-03-13 03:43:32 -0400

    Falcon.lua & pump.lua - Tweaked the programs visually and mentally ^_^
    Left copy of pump.lua as pump.lua.old for historic purposes.

--- VeggieTales/luaScripts/falcon.lua
+++ VeggieTales/luaScripts/falcon.lua
@@ -4,28 +4,51 @@
 
 assert(loadfile("luaScripts/common.inc"))();
 
+delay = 100;
+
 function doit()
-	askForWindow('Macro looks for windows while searching falcons. Simply right click a tree while the macro is running. Macro will click "Search for Falcon Roosts" for you. Then it will wait 2.5 seconds and close the popup box (OK Button) for you. All you have to do is right click trees, the macro will do the rest of the clicking for you!');
+	askForWindow('Falcon Assistant v1.0 by Cegaiel\n\nMacro will click "Search for Falcon Roosts" window. Then it will wait 1 second (so you can read) and close the popup box (OK Button) for you.\n\nIf the falcon is found, macro will quit, play a sound and display a message.\n\nAll you have to do is click trees, the macro will do the rest of the clicking for you!\n\nOptions, One-Click, Fast Gather Wood MUST be UNChecked!\n\nPress Shift over ATITD to continue.');
 
 	while 1 do
 		checkBreak();
 		srReadScreen();
-		sleepWithStatus(100, "Searching for Falcon message");
-		local falcon = srFindImage("falcon.png");
+		local falcon = srFindImage("falcon.png"); 			-- "Search for Roost" message when you click a tree
+		local falconfound = srFindImage("falcon2.png"); 		-- Popup after clicking a tree AND You found a falcon
+		local ok = srFindImage("OK.png"); 					--  Popup after clicking a tree.
+		local tired = srFindImage("Endurance-Red.png"); 		-- Verifies tired and skills window is showing
+		local skills = srFindImage("dexterity_black_small.png"); 	-- Verifies skill window is showing. Use Dex (black) to verify because Endurance will change colors
+
+
+
+			if falconfound then
+			sleepWithStatus(15000, "YOU FOUND YOUR FALCON!\n\nCONGRATULATIONS!!!");
+			lsPlaySound("Complete.wav");
+			error("You found your falcon! Congratulations!!!");
+			end
+
+
+			if ok then
+			sleepWithStatus(1000, "Preparing to click OK button..."); -- Wait a bit before clicking OK, so user has chance to read the popup.
+			srClickMouseNoMove(ok[0]+5,ok[1],1);
+			lsSleep(delay);
+			end
+
+
 			if falcon then
 			srClickMouseNoMove(falcon[0]+5,falcon[1],1);
-
-				while not ok do
-				checkBreak();
-				srReadScreen();
-				local ok = srFindImage("OK.png");
-					if ok then
-					sleepWithStatus(2500, "Preparing to click OK button"); -- Wait a bit before clicking OK, so user has chance to see the "Falcon was caught" message.
-					srClickMouseNoMove(ok[0]+5,ok[1],1);
-					break;
-					end
-				end
+			lsSleep(delay);
 			end
+
+
+			if tired then
+				sleepWithStatus(delay, "Searching for Falcon Roost...\n\nWAIT, You are tired!", 0xff8080ff);
+			elseif not skills then
+				sleepWithStatus(delay, "Searching for Falcon Roost...\n\nWARNING, Can not find skills window!", 0xffff40ff);
+			else
+				sleepWithStatus(delay, "Searching for Falcon Roost...\n\nREADY, Click a tree!", 0x40ff40ff);
+
+			end
+			
 
 	end
 end
