c80d0d54257e456ffd08edfd3a3a26f33987459d
Cegaiel <sharpguy32@yahoo.com>
2014-03-28 04:59:07 -0400

    Fishing.lua and click_max.lua update
    click_max.lua- removed an extra delay not needed, clicks button very quickly now. Makes dropping multiple items a breeze now.
    
    Fishing.lua - Updated the "Fun Pack" sound effects scheme. Rearranged the sound effects. Sounds better and overlapping sound effects "mix" very nicely now :)

--- VeggieTales/luaScripts/click_max.lua
+++ VeggieTales/luaScripts/click_max.lua
@@ -5,16 +5,17 @@
 assert(loadfile("luaScripts/common.inc"))();
 
 function doit()
-	askForWindow('Searches for and clicks Max button until stopped.\n \nGreat for filling grass hammocks, loading distaffs, etc quickly.\n \nNot so good for dividing straw amongst many camel pens.\n \nPress Shift over ATITD window to continue.');
+	askForWindow('Searches for and clicks Max button until stopped.\n \nGreat for filling grass hammocks, loading distaffs, dropping many itmes, etc quickly.\n \nNot so good for dividing straw amongst many camel pens.\n \nPress Shift over ATITD window to continue.');
 
 	while 1 do
 		checkBreak();
 		srReadScreen();
-		sleepWithStatus(100, "Searching for Max button");
 		local max = srFindImage("crem-max.png");
 			if max then
 			srClickMouseNoMove(max[0]+5,max[1],1);
-			sleepWithStatus(500, "Clicking Max button");
+			sleepWithStatus(100, "Clicking Max button");
+			else
+			sleepWithStatus(100, "Searching for Max button");
 			end
 	end
 end
