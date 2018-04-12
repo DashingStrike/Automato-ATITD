4abad9b05e70835aa677e87d1916b12cbbaa0da2
Cegaiel <sharpguy32@yahoo.com>
2018-04-12 04:50:46 -0400

    test_ocr_clock.lua -- More Robust
    Script can now detect if you are running the new T8 Clock or Legacy. It
    can also detect which Interface Size (of either Legacy or T8 Clock)
    
    This is should be a great diagnostic tool to report to player if they
    are running New T8 Clock and/or if they are running the wrong Interface
    Size.

--- scripts/test_ocr_clock.lua
+++ scripts/test_ocr_clock.lua
@@ -1,4 +1,28 @@
 dofile("common.inc");
+	
+
+--Check to see if player has new T8 clock displaying
+function findNewClock(size)
+  local result = nil;
+    if not size then
+      size = "Small";
+    end
+  srReadScreen();
+  result = srFindImage("clockT8_" .. size .. ".png",5000);
+  return result;
+end
+
+
+--Check to see if player has Legacy clock displaying, but wrong interface size
+function findLegacyClock(size)
+  local result = nil;
+    if not size then
+      size = "Small";
+    end
+  srReadScreen();
+  result = srFindImage("clockLegacy_" .. size .. ".png",5000);
+  return result;
+end
 
 
 function doit()
@@ -17,6 +41,17 @@ duckTime = 0;
 duckTimeSeconds = 0;
 duckTimeAvg = 0;
 minsElapsedGUI = "Waiting change";
+message = "";
+message2 = "";
+
+clockSize = {
+"Tiny",
+"Small",
+"Medium",
+"Large",
+"Huge",
+};
+
 
 	findClockInfo();
 
@@ -57,19 +92,50 @@ minsElapsedGUI = "Waiting change";
 		while not Time do
 		  checkBreak();
 		  findClockInfo();
-		  sleepWithStatus(1000, "Can not find Clock!\n\nMake sure your clock and all 4 clock borders are visible.\n\nStart moving your clock around until macro resumes...");
+		  message = "";
+		  message1 = "";
+		  message1b = "";
+		  message2 = "";
+		  message2b = "";
+
+			for i=1,#clockSize do
+			  checkBreak();
+			  findClock = findNewClock(clockSize[i]);
+			    if findClock then
+				message1 = "It appears you have the new (T8) clock showing! However, this macro requires the legacy clock.\n\nIn fact, all macros in Automato requires the legacy clock.\n\nUnder Options, Interface-Options, Actions: Use Legacy Interface";
+				message1b = "\n\nYou are using \"" .. clockSize[i] .. "\" Interface";
+			    end
+			end
+
+			for i=1,#clockSize do
+
+			  checkBreak();
+			  findClock2 = findLegacyClock(clockSize[i]);
+			    if findClock2 == "Small" then
+	 		      message2 = "Can not find Clock!\n\nIf you have no clock displaying, type /clockinfo in game.\n\nMake sure your clock and all 4 clock borders are visible.\n\nTry moving your clock until macro resumes...";
+			    elseif findClock2 and findClock2 ~= "Small" then
+	 		      message2 = "Found Legacy Clock, but wrong interface size.\n\nAll macros in Automato requires the \"Small\" interface size.\n\nCheck Options, Interface-Options, Interface Size: Interface is \"Small\".";
+			      message2b = "\n\nYou are using \"" .. clockSize[i] .. "\" Interface";
+			    end
+
+			    if  message1 == "" and message2 == "" then
+			      message2 = "Can not find Clock!\n\nIf you have no clock displaying, type /clockinfo in game.\n\nMake sure your clock and all 4 clock borders are visible.\n\nTry moving your clock until macro resumes...";
+			    end
+
+			end
+
+		  sleepWithStatus(200, message1 .. message1b .. message2 .. message2b, nil, 0.7);
 		end
-      statusScreenSmall("Mouse Pos: " .. pos[0] .. ", " .. pos[1] .. "\n\nYear: " .. year .. "\nDate: " .. Date .. "\nTime: " .. Time .. "\nRegion: " .. regionName .. "\nCoords: " 
+
+      statusScreen("Mouse Pos: " .. pos[0] .. ", " .. pos[1] .. "\n\nYear: " .. year .. "\nDate: " .. Date .. "\nTime: " .. Time .. "\nRegion: " .. regionName .. "\nCoords: " 
 	  .. Coordinates .. "\nFaction: " .. faction .. "\n\nElapsed Egypt Mins: " .. minsElapsedGUI .. "\nLast Secs per Egypt Minute: " .. elapsedTimeSeconds .. "\nAvg Secs per Egypt Minute: " .. elapsedTimeAvg .. "\nEstimated Last Duck Time: " 
-	  .. duckTimeSeconds .. "\nEstimated Duck Time Avg: " .. duckTimeAvg);
-      lsSleep(10);
+	  .. duckTimeSeconds .. "\nEstimated Duck Time Avg: " .. duckTimeAvg, nil, nil, 0.7);
 
-
-		if faction == "Hyksos"  then
+		if faction == "Hyksos" or faction == " Hyksos" then
 		  lsButtonImg(170, 110, 1, 0.20, 0xFFFFFFff, "factions/hyksos.png");
-		elseif faction == "Kush" then
+		elseif faction == "Kush" or faction == " Kush" then
 		  lsButtonImg(170, 110, 1, 0.17, 0xFFFFFFff, "factions/kush.png");
-		elseif faction == "Meshwesh" then
+		elseif faction == "Meshwesh" or faction == " Meshwesh" then
 		  lsButtonImg(170, 110, 1, 0.20, 0xFFFFFFff, "factions/hyksos.png");
 		end
 	end
@@ -130,41 +196,8 @@ Time = nil;
 	regionInfo = string.sub(regionInfo,string.find(regionInfo,",")+1);
 	Coordinates = string.sub(regionInfo,string.find(regionInfo,":") + 2);
 	regionName = string.sub(regionInfo, 1,( string.len(regionInfo) - string.len(Coordinates) -2));
-	faction = table.concat(lines[3], ",") -- Line 3 on the clock
-	faction = string.sub(faction, 14);
+	faction = table.concat(lines[3]) -- Line 3 on the clock
+	faction = string.sub(faction, 13);
     end
   end
-end
-
-
-function statusScreenSmall(message, color, allow_break)
-  if not message then
-    message = "";
-  end
-  if not color then
-    color = 0xFFFFFFff;
-  end
-  if allow_break == nil then
-    allow_break = true;
-  end
-  lsSetCamera(0,0,lsScreenX*1.1,lsScreenY*1.1);
-  lsPrintWrapped(10, 80, 0, lsScreenX - 20, 0.8, 0.8, color, message);
-  lsPrintWrapped(10, lsScreenY-100, 0, lsScreenX - 20, 0.8, 0.8, 0xffd0d0ff,
-		 error_status);
-  lsSetCamera(0,0,lsScreenX*1.0,lsScreenY*1.0);
-  if lsButtonText(lsScreenX - 110, lsScreenY - 30, z, 100,
-		  0xFFFFFFff, "End script") then
-    error(quit_message);
-  end
-  if allow_break then
-    lsPrint(10, 10, 0, 0.7, 0.7, 0xB0B0B0ff,
-	    "Hold Ctrl+Shift to end this script.");
-    if allow_pause then
-      lsPrint(10, 24, 0, 0.7, 0.7, 0xB0B0B0ff,
-	      "Hold Alt+Shift to pause this script.");
-    end
-    checkBreak();
-  end
-  lsSleep(tick_delay);
-  lsDoFrame();
 end
