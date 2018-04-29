2c6e155afe437bae53547c9de0cacf980517546b
ChristophRau <chrau@ucla.edu>
2018-04-29 09:10:47 -0700

    chem_notepad - replace csplit w/ Explode

--- scripts/chem_notepad.lua
+++ scripts/chem_notepad.lua
@@ -68,11 +68,13 @@ function browseMenu(tree)
 end
 
 function setLine(tree, line)
-  local sections = csplit(line, "|");
+  --local sections = csplit(line, "|");
+  local sections = explode("|",line);
   if #sections ~= 2 then
     error("Cannot parse line: " .. line);
   end
-  local tags = csplit(sections[1], ",");
+  --local tags = csplit(sections[1], ",");
+  local tags = csplit(",",sections[1]);
   local data = sections[2];
   setData(tree, tags, data);
 end
@@ -155,3 +157,25 @@ function saveNotes(filename, tree)
   end
   io.close(file);
 end
+
+-- Added in an explode function (delimiter, string) to deal with broken csplit.
+function explode(d,p)
+   local t, ll
+   t={}
+   ll=0
+   if(#p == 1) then
+      return {p}
+   end
+   while true do
+      l = string.find(p, d, ll, true) -- find the next d in the string
+      if l ~= nil then -- if "not not" found then..
+         table.insert(t, string.sub(p,ll,l-1)) -- Save it in our array.
+         ll = l + 1 -- save just after where we found it for searching next time.
+      else
+         table.insert(t, string.sub(p,ll)) -- Save what's left in our array.
+         break -- Break at end, as it should be, according to the lua manual.
+      end
+   end
+   return t
+end
+
