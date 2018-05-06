c505c354c901112da60b9bb56b0762cf8a824545
Cegaiel <sharpguy32@yahoo.com>
2018-05-06 17:24:55 -0400

    cc_Auto.lua - Stats added
    Macro now displays various statistics while running.
    
    Such as how much wood you've used and water used. It tracks it by per pass and total passes.
    
    This could also be useful in fine tuning macro wood  usage (currently/likely higher than it should be)...

--- scripts/cc_Auto.lua
+++ scripts/cc_Auto.lua
@@ -19,15 +19,25 @@ CLOSE = 4;
 OPEN = 5;
 FULL = 6;
 
+woodAddedTotal = 0;
+waterAddedTotal = 0;
+
 function ccMenu()
   local passCount = promptNumber("How many passes?", 1, 1.0);
   askForFocus();
+  startTime = lsGetTimer();
   for i=1,passCount do
+	woodAdded = 0;
+	waterAdded = 0;
+	woodx1Click = 0;
+    Do_Take_All_Click(); -- Make sure ovens are empty. If a previous run didn't complete and has wood leftover, will cause a popup 'Your oven already has wood' and throw macro off
     runCommand(buttons[1]);
     lsSleep(1000);
     ccRun(i, passCount);
   end
+  Do_Take_All_Click(); -- All done, empty ovens
   lsPlaySound("Complete.wav");
+  lsMessageBox("Elapsed Time:", getElapsedTime(startTime), 1);
 end
 
 function findOvens()
@@ -55,11 +65,20 @@ function findButton(pos, index)
   return findImageInWindow(buttons[index].image, pos[0], pos[1]);
 end
 
-function clickButton(pos, index)
+function clickButton(pos, index, counter)
   local buttonPos = findButton(pos, index);
   if buttonPos then
     safeClick(buttonPos[0] + buttons[index].offset[0],
 	      buttonPos[1] + buttons[index].offset[1]);
+
+	if index == 3 and counter ~= nil then -- Water
+	  waterAdded = waterAdded + 1;
+	  waterAddedTotal = waterAddedTotal + 1;
+	end
+	if index == 2 and counter ~= nil then -- Wood
+	  woodAdded = woodAdded + 1;
+	  woodAddedTotal = woodAddedTotal + 1;
+	end
   end
 end
 
@@ -68,8 +87,9 @@ function ccRun(pass, passCount)
   local vents = setupVents(ovens);
   local done = false;
   while not done do
-    sleepWithStatus(2000, "(" .. pass .. " / " .. passCount .. ")\n" ..
-		    "Waiting for next tick");
+    sleepWithStatus(2000, "Waiting on next tick ...\n\n[" .. pass .. "/" .. passCount .. "] Passes\n\nTotals: [This Pass/All Passes]\n\n[".. woodAdded*#ovens*3 .. "/" .. 
+		woodAddedTotal*#ovens*3 .. "] Wood Used - Actual\n[" .. woodAdded.. "/" .. woodAddedTotal .. "] 'Add Wood' Button Clicked (x1)\n\n[" .. waterAdded*#ovens .. "/" .. waterAddedTotal*#ovens .. 
+		"] Water Used\n             (Excluding cooldown water)\n\n\nElapsed Time: " .. getElapsedTime(startTime),nil, 0.7, 0.7);
     done = true;
   	srReadScreen();
     for i=1,#ovens do
@@ -83,18 +103,18 @@ end
 
 -- 0% = 56, 100% = 249, each % = 1.94
 
-minHeat = makePoint(199, 15-0);
-minHeatProgress = makePoint(152, 15-0);
-minOxy = makePoint(80, 33-0);
-maxOxy = makePoint(116, 33-0);
-maxOxyLowHeat = makePoint(160, 33-0);
-minWood = makePoint(108, 50-0);
-minWater = makePoint(58, 70-0);
-minWaterGreen = makePoint(96, 70-0);
-maxDangerGreen = makePoint(205, 90-0);
-maxDanger = makePoint(219, 90-0);
-uberDanger = makePoint(228, 90-0);
-progressGreen = makePoint(62, 110-0);
+minHeat = makePoint(199, 15);
+minHeatProgress = makePoint(152, 15);
+minOxy = makePoint(80, 33);
+maxOxy = makePoint(116, 33);
+maxOxyLowHeat = makePoint(160, 33);
+minWood = makePoint(108, 50);
+minWater = makePoint(58, 70);
+minWaterGreen = makePoint(96, 70);
+maxDangerGreen = makePoint(205, 90);
+maxDanger = makePoint(219, 90);
+uberDanger = makePoint(228, 90);
+progressGreen = makePoint(62, 110);
 
 greenColor = 0x07FE05;
 barColor = 0x0606FD;
@@ -103,6 +123,7 @@ function processOven(oven, vent)
   local newVent = vent;
   if pixelMatch(oven, progressGreen, greenColor, 4) then
     -- Progress is green
+
     if not pixelMatch(oven, minWaterGreen, barColor, 8) then
       -- Aggressively add water
       clickButton(oven, WATER);
@@ -115,13 +136,16 @@ function processOven(oven, vent)
       newVent = 3;
       clickButton(oven, FULL);
     end
+
+
   else
+
     -- Progress is not green
     if not pixelMatch(oven, minHeat, barColor, 8) then
       -- Heat too low
       if not pixelMatch(oven, minWood, barColor, 8) then
 	-- Wood too low
-	clickButton(oven, WOOD);
+	clickButton(oven, WOOD, 1);
       end
     end
 
@@ -155,13 +179,57 @@ function processOven(oven, vent)
       -- Danger > 85%
       if not pixelMatch(oven, minWater, barColor, 8) then
 	-- Water < 2.6%
-	clickButton(oven, WATER);
+	clickButton(oven, WATER, 1);
 	-- Splash more water if it > 90%
 	if pixelMatch(oven, uberDanger, barColor, 8) then
-	  clickButton(oven, WATER);
+	  clickButton(oven, WATER, 1);
 	end
       end
     end
   end
   return newVent;
+end
+
+function Do_Take_All_Click()
+      statusScreen("Checking / Emptying Ovens ...",nil, 0.7, 0.7);
+	-- refresh windows
+	clickAll("This.png", 1);
+	lsSleep(100);
+
+	clickAll("Take.png", 1);
+	lsSleep(100);
+	
+	clickAll("Everything.png", 1);
+	lsSleep(100);
+	
+	-- refresh windows, one last time so we know for sure the machine is empty (Take menu disappears)
+	clickAll("This.png", 1);
+	lsSleep(100);
+end
+
+function clickAll(image_name)
+	-- Find buttons and click them!
+	srReadScreen();
+	xyWindowSize = srGetWindowSize();
+	local buttons = findAllImages(image_name);
+	
+	if #buttons == 0 then
+		-- statusScreen("Could not find specified buttons...");
+		-- lsSleep(1500);
+	else
+		-- statusScreen("Clicking " .. #buttons .. "button(s)...");
+		if up then
+			for i=#buttons, 1, -1  do
+				srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3);
+				lsSleep(per_click_delay);
+			end
+		else
+			for i=1, #buttons  do
+				srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3);
+				lsSleep(per_click_delay);
+			end
+		end
+		-- statusScreen("Done clicking (" .. #buttons .. " clicks).");
+		-- lsSleep(100);
+	end
 end
