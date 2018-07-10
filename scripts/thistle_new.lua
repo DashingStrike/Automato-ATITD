9f78559dcece86e3be1bea65dfa05497f4923469
Cegaiel <sharpguy32@yahoo.com>
2018-07-10 16:07:11 -0400

    Revert "thistle_new.lua - Fix nested functions so that Back button returns to top parent"
    This reverts commit 9b63d7e44cf4918bdce61f13b34921d2ac5f264f.

--- scripts/thistle_new.lua
+++ scripts/thistle_new.lua
@@ -405,8 +405,7 @@ function config()
     "\n\nWe are ready to start making thistles, with this farm\'s recipe! Click Start button to proceed ..."); 
 
     if lsButtonText(lsScreenX - 110, lsScreenY - 60, 0, 100, 0xFFFFFFff, "Back") then
---	thistleConfig();
-	break;
+	thistleConfig();
     end
 
     if lsButtonText(10, lsScreenY - 30, 0, 100, 0xFFFFFFff, "Start") then
@@ -571,8 +570,7 @@ function miscButtons()
   end
 
   if lsButtonText(10, lsScreenY - 30, 0, 100, 0xFFFFFFff, "Back") then
---    thistleConfig();
-	break;
+    thistleConfig();
   end
 
   if lsButtonText(lsScreenX - 110, lsScreenY - 30, z, 100, 0xFFFFFFff, "End script") then
@@ -755,15 +753,14 @@ function fetchVoids()
     statusScreen(result, nil, 0.65,0.65);
     if lsButtonText(10, lsScreenY - 30, 0, 100, 0xFFFFFFff, "Back") then
 	socketHttpFirstRun = 1;
---	thistleConfig();
-	break;
+	thistleConfig();
     end
     lsSleep(10);
   end
 end
 
 function fetchVoidsWarning()
-  while 1 and not socketHttpFirstRun do
+  while 1 do
     checkBreak();
     lsPrintWrapped(10, 10, 0, lsScreenX - 20, 0.7, 0.7, 0xFFFFFFff,
       "You are about to fetch a web page from Cegaiel\'s ATITD Tools page.\n\n" ..
@@ -772,8 +769,7 @@ function fetchVoidsWarning()
 
 
     if lsButtonText(lsScreenX - 110, lsScreenY - 60, 0, 100, 0xFFFFFFff, "Back") then
---      thistleConfig();
-	break;
+      thistleConfig();
     end
     if lsButtonText(10, lsScreenY - 30, 0, 120, 0xFFFFFFFF, "Check Voids") then
       fetchVoids();
@@ -840,8 +836,7 @@ function promptRecipe(farm)
     end
 
     if lsButtonText(lsScreenX - 110, lsScreenY - 60, 0, 100, 0xFFFFFFff, "Back") then
---      thistleConfig();
-	break;
+      thistleConfig();
     end
 
     local badList = false;
