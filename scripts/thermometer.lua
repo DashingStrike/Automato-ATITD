b53d0d2fceec6a2b861849471db9eae9b052ca32
Cegaiel <sharpguy32@yahoo.com>
2018-07-14 03:01:45 -0400

    thermometer.lua - See notes
    Show current pass in status message
    
    Add 5 second countdown and warning about you should not move mouse and to make sure chat is minimized. Give a chance to abort, before starting.
    
    Force mouse to hover glory hole before each SendKeyEvent - in case you forget you're not suppose to move mouse.
    
    Adjust scale to 0.7 in all statusScreen(messages)
    
    Slight tweak in statusScreen(messages)
    
    Shows how many keystrokes are remaining

--- scripts/thermometer.lua
+++ scripts/thermometer.lua
@@ -48,9 +48,9 @@ function clickAll(image_name, up)
 	local buttons = findAllImages(image_name);
 	
 	if #buttons == 0 then
-		statusScreen("Could not find " .. image_name);
+		statusScreen("Could not find " .. image_name, nil, 0.7, 0.7);
 	else
-		statusScreen("Clicking " .. #buttons .. "button(s)...");
+		statusScreen("Clicking " .. #buttons .. "button(s)...", nil, 0.7, 0.7);
 		if up then
 			for i=#buttons, 1, -1  do
 				srClickMouseNoMove(buttons[i][0]+2, buttons[i][1]+2);
@@ -62,35 +62,39 @@ function clickAll(image_name, up)
 				lsSleep(per_click_delay);
 			end
 		end
-		statusScreen("Done clicking (" .. #buttons .. " clicks).");
+		statusScreen("Done clicking (" .. #buttons .. " clicks).", nil, 0.7, 0.7);
 		lsSleep(150);
 	end
 end
 
 recipe = "aaaaaaaaaaaaaaaaammmmmmmmmmmmmmmmmmmbmmbmmbmmbmmmqm"
 
-
+recipeLen = string.len(recipe);
 keyDelay = 150;
 function doit()
 	local num_rounds;
+	local keystrokes;
 	num_rounds = promptNumber("How many thermos?", 1);
 	askForWindowAndPixel("                        Pin up the 'Start Making' menu. Make sure your chat is minimized! Click in ATITD, hover your mouse over the glory hole and push shift.  Ensure your heater control is set to Standard.");
+	sleepWithStatus(5000, "Preparing to start macro!\n\nDon\'t touch mouse, EVER, while running!\n\nAre you sure chat is minimized?\n\nNow is your chance to Abort if not...", nil, 0.7, 0.7);
 	for i = 1, num_rounds do
+		keystrokes = 0;
 		status = "";
 		checkBreak();
 		clickAll("Thermometer.png", true);
 		lsSleep(100);
 		srSetMousePos(mouse_x, mouse_y);
-		statusScreen("Please be patient, the macro is sending keys to the glory hole",nil, 0.7, 0.7);
 		for j = 1, string.len(recipe) do
 			--recipe time
             checkBreak();
             local currKey = string.sub(recipe, j, j);
+            srSetMousePos(mouse_x, mouse_y);
             srKeyEvent(currKey);
+            keystrokes = keystrokes + 1;
             status = status .. currKey;
 			lsSleep(keyDelay);
 
-            statusScreen("Please be patient, the macro is sending keys to the glory hole\n\n" .. status,nil, 0.7, 0.7);
+            statusScreen("[" .. i .. "/" .. num_rounds .. "] Thermometer(s)\n[" .. keystrokes .. "/" .. recipeLen .. "] Keystrokes remaining\n\nPlease be patient; Don\'t touch mouse!\nSending keystrokes to the glory hole...\n\n" .. status,nil, 0.7, 0.7);
 
 		end
 		lsSleep(keyDelay);
