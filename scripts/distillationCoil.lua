57f4cb00b3841c0333ee44f8e47c2ef6beab7dc8
Cegaiel <sharpguy32@yahoo.com>
2018-07-14 03:02:40 -0400

    distillationCoil.lua - See notes
    Show current pass in status message
    
    Add 5 second countdown and warning about you should not move mouse and to make sure chat is minimized. Give a chance to abort, before starting.
    
    Force mouse to hover glory hole before each SendKeyEvent - in case you forget you're not suppose to move mouse.
    
    Adjust scale to 0.7 in all statusScreen(messages)
    
    Slight tweak in statusScreen(messages)
    
    Shows how many keystrokes are remaining

--- scripts/distillationCoil.lua
+++ scripts/distillationCoil.lua
@@ -47,9 +47,9 @@ function clickAll(image_name, up)
 	local buttons = findAllImages(image_name);
 	
 	if #buttons == 0 then
-		statusScreen("Could not find " .. image_name);
+		statusScreen("Could not find " .. image_name, nil, 0.7, 0.7);
 	else
-		statusScreen("Clicking " .. #buttons .. "button(s)...");
+		statusScreen("Clicking " .. #buttons .. "button(s)...", nil, 0.7, 0.7);
 		if up then
 			for i=#buttons, 1, -1  do
 				srClickMouseNoMove(buttons[i][0]+2, buttons[i][1]+2);
@@ -61,7 +61,7 @@ function clickAll(image_name, up)
 				lsSleep(per_click_delay);
 			end
 		end
-		statusScreen("Done clicking (" .. #buttons .. " clicks).");
+		statusScreen("Done clicking (" .. #buttons .. " clicks).", nil, 0.7, 0.7);
 		lsSleep(150);
 	end
 end
@@ -69,52 +69,61 @@ end
 recipe = "aaaawwwwannnwwawwannnwwawwannnwwawwannnwwawwannnwwawwannnwwwwannnwwawwannnwwawwannnwwawwannnwwawwaaaaaaaaaaaannnwwwnnnwwwwnnnwwwndndndndndndndndndndndndndndndndndndndndndndndndndndndndndndnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpnpndndndndndndndndndndndndndndndndn";
 
 
+
+recipeLen = 204; -- 204 is without non glory hole keys w,d,q,p,i
 keyDelay = 150;
 function doit()
 	local num_rounds;
+	local keystrokes;
 	num_rounds = 1; -- promptNumber("How many coils?", 1);
 	askForWindowAndPixel("                        Pin up the 'Start Making' menu. Make sure your chat is minimized! Click in ATITD, hover your mouse over the glory hole and push shift.  Ensure your heater control is set to Pinpoint.");
-
+	sleepWithStatus(5000, "Preparing to start macro!\n\nDon\'t touch mouse, EVER, while running!\n\nAre you sure chat is minimized?\n\nNow is your chance to Abort if not...", nil, 0.7, 0.7);
 	for i = 1, num_rounds do
+		keystrokes = 0;
 		status = "";
+		checkBreak();
 		clickAll("DistillationCoil.png", true);
 		lsSleep(100);
-		statusScreen("Please be patient, the macro is sending keys to the glory hole",nil, 0.7, 0.7);
-		srSetMousePos(mouse_x, mouse_y);
 		for j = 1, string.len(recipe) do
 			--recipe time
             local currKey = string.sub(recipe, j, j);
+            srSetMousePos(mouse_x, mouse_y);
             if currKey == "w" then
                 lsSleep(998);
                 srKeyEvent("q");
-                status = status .. "q";
+                --status = status .. "q";
             elseif currKey == "d" then
                 lsSleep(249);
                 srKeyEvent("q");
-                status = status .. "q";
+                --status = status .. "q";
             elseif currKey == "p" then
                 lsSleep(125);
                 srKeyEvent("q");
-                status = status .. "q";
+                --status = status .. "q";
             elseif currKey == "i" then
                 lsSleep(75);
             else
                 srKeyEvent(currKey);
                 status = status .. currKey;
-
+                keystrokes = keystrokes + 1;
                 if currKey ~= "a" then
                     srKeyEvent("q");
-                	status = status .. "q";
+                	--status = status .. "q";
                 end
             end
 
-            statusScreen("Please be patient, the macro is sending keys to the glory hole\n\n" .. status,nil, 0.7, 0.7);
-            checkBreak();
-
+		statusScreen("[" .. keystrokes .. "/" .. recipeLen .. "] Keystrokes remaining\n\nPlease be patient; Don\'t touch mouse!\n\nSending keystrokes to the glory hole...\n\n" .. status, nil, 0.7, 0.7);
 
             
 			--lsSleep(keyDelay);
 		end
+
+		while 1 do
+		  checkBreak();
+		  sleepWithStatus(100,"Distillation Coil is done, however you need to manually [U]nload it.\n\nThis gives you an opportunity to keep checking quality or furthur fine tuning.", nil, 0.7, 0.7);
+		end
+
+
 	--		lsSleep(400);
 	--		srKeyEvent("u");
 	--	clickAll("Blank.png");
