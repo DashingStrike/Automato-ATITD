e02773ba33164dfabdba9f16e9882a7f3b479854
Cegaiel <sharpguy32@yahoo.com>
2018-08-10 04:55:18 -0400

    Update WH color to match the new warehouse roof, recently updated in game.

--- scripts/gather.lua
+++ scripts/gather.lua
@@ -180,7 +180,7 @@ WaypointTypes[Trilobellia] = "Trilobellia";
 
 WaypointColors = {};
 WaypointColors[Bonfire] = 1060316671;
-WaypointColors[Warehouse] = 1599225343;
+WaypointColors[Warehouse] = 2691905791;
 WaypointColors[MenuClick] = 0;
 WaypointColors[ToweringPalm] = 1585197055;
 WaypointColors[RoyalPalm] = 137495039;
@@ -873,7 +873,7 @@ function fillJugs()
         lsSleep(1000);
         srReadScreen();
         clickMax();
-		emptyJugs = 0;
+        emptyJugs = 0;
         lsSleep(2500);
         return true;
     end
