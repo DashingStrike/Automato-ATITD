8a9fddc09f69676d3d2afc69ba5f41832517b2fc
Cegaiel <sharpguy32@yahoo.com>
2018-12-26 21:31:12 -0500

    Fix: silt.lua - Delete: test_analyze_custom.lua

--- scripts/silt.lua
+++ scripts/silt.lua
@@ -1,14 +1,17 @@
-dofile("screen_reader_common.inc");
-dofile("ui_utils.inc");
+dofile("common.inc");
 
-local distance_scale = 0.11; -- only scans pixels within a distance of this percentage from the center of the screen
+local distance_scale = 0.50; -- only scans pixels within a distance of this percentage from the center of the screen
+
+
 
 function getCenterPos()
+	xyWindowSize = srGetWindowSize();
 	local ret = {};
 	ret[0] = xyWindowSize[0] / 2;
 	ret[1] = xyWindowSize[1] / 2;
 	return ret;
 end
+
 
 function isWithinRange(x, y)
 	local radius = xyWindowSize[0] * 0.15;
@@ -23,67 +26,59 @@ function isWithinRange(x, y)
 end
 
 
-function clickAll(image_name, up, right_click)
-	-- Find buttons and click them!
+function clickMouseSplatter(x, y, right)
+	--srSetMousePos(x, y);
+	srClickMouse(x + math.random(-2, 2), y + math.random(-2, 2), right);
 	srReadScreen();
-	xyWindowSize = srGetWindowSize();
-	local buttons = findAllImages(image_name);
-	
-	if #buttons == 0 then
-		-- statusScreen("Could not find specified buttons...");
-		-- lsSleep(1500);
-	else
-		--statusScreen("Clicking " .. #buttons .. "button(s)...");
-		if up then
-			for i=#buttons, 1, -1  do
-				if (buttons[i][0] > 200) then
-					srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3, right_click);
-					lsSleep(per_click_delay);
-				end
-			end
-		else
-			for i=1, #buttons  do
-				srClickMouseNoMove(buttons[i][0]+5, buttons[i][1]+3, right_click);
-				lsSleep(per_click_delay);
-			end
-		end
-		--statusScreen("Done clicking (" .. #buttons .. " clicks).");
-		--lsSleep(100);
+	unpin = srFindImage("UnPin.png")
+	if unpin then
+	  clickAllImages("Unpin.png", 5, 3, 1);
 	end
 end
 
-function clickMouseSplatter(x, y, right)
-	-- srSetMousePos(x, y);
-	srClickMouse(x + math.random(-2, 2), y + math.random(-2, 2), right);
+
+function clickAllImages(image_name, offsetX, offsetY, rightClick, tol)
+  if not image_name then
+    error("Incorrect number of arguments for clickAllImages()");
+  end
+
+  srReadScreen();
+  local buttons = findAllImages(image_name, nil, tol);
+  clickAllPoints(buttons, offsetX, offsetY, rightClick);
+  return #buttons;
 end
 
-function doit()
-	askForWindow("Use F5 camera zoomed out about half way.  Run around with arrows keys after macro begins, stop moving if it fails to pick something.  Interface Options | Right-Click opens a Menu as Pinned must be ON.  Video | Time-of-Day Lighting must be at LOWEST (off).");
 
+function doit()
+	askForWindow("Click Silt while you run around.\nRequires Automato v2.32 or above.\n\nUse F5 camera zoomed out about half way.\n\nRun around with arrows keys after macro begins, stop moving if it fails to pick something.\n\nInterface Options | Right-Click opens a Menu as Pinned must be ON.\n\nVideo | Time-of-Day Lighting must be at LOWEST (off).");
+
+
+	getCenterPos();
 	srReadScreen();
 
 	while true do
 		local frame_start = lsGetTimer();
-		statusScreen("Watching for silt");
-		possible_papy = lsAnalyzeSilt(7, 40, 1, xyWindowSize[0] * distance_scale);
+		statusScreen("Watching for Silt (Red Dots)", nil, 0.7, 0.7);
+		-- Looks for pixels whose red is between 0xA0 and 0xFF (160-255), and green/blue are less than 0x60
+		clusters = lsAnalyzeCustom(7, 40, 1, xyWindowSize[0] * distance_scale, 0xcac2b7FF, 0xe7e2d9FF, true);
 		local clicked = nil;
-		if possible_papy then
-			for i = 1, #possible_papy do
+		if clusters then
+			for i = 1, #clusters do
 				-- bias because it seems to drag to the upper left, take this off
 				--  if I fix the downsampling
-				possible_papy[i][0] = possible_papy[i][0] + 5;
-				--possible_papy[i][1] = possible_papy[i][1] + 5;
-				if isWithinRange(possible_papy[i][0], possible_papy[i][1]) then
+				clusters[i][0] = clusters[i][0] + 5;
+				--clusters[i][1] = clusters[i][1] + 5;
+				if isWithinRange(clusters[i][0], clusters[i][1]) then
 				
-					-- srClickMouse(possible_papy[i][0], possible_papy[i][1], 1);
-					-- srSetMousePos(possible_papy[i][0], possible_papy[i][1]);
+					-- srClickMouse(clusters[i][0], clusters[i][1], 1);
+					-- srSetMousePos(clusters[i][0], clusters[i][1]);
 
 					-- code to try to make sure the pixel is the right color
 					-- off by a few pixels in translation, maybe?  needs to be pixel accurate!
 					local good;
 					local r, g, b;
 					if nil then
-						pxval = srReadPixel(possible_papy[i][0], possible_papy[i][1]);
+						pxval = srReadPixel(clusters[i][0], clusters[i][1]);
 						a = pxval % 256;
 						pxval = (pxval - a) / 256;
 						b = pxval % 256;
@@ -102,24 +97,18 @@ function doit()
 					end
 					
 					if good then
-						lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0xFFFFFFff, possible_papy[i][0] .. ", " .. possible_papy[i][1] .. " : " .. r .. "," .. g .. "," .. b);
-						clickMouseSplatter(possible_papy[i][0], possible_papy[i][1], 1);
+						lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0xFFFFFFff, clusters[i][0] .. ", " .. clusters[i][1] .. " : " .. r .. "," .. g .. "," .. b);
+						clickMouseSplatter(clusters[i][0], clusters[i][1], 1);
 						clicked = 1;
 					else
-						lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0xFF7F7Fff, possible_papy[i][0] .. ", " .. possible_papy[i][1] .. " : " .. r .. "," .. g .. "," .. b);
+						lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0xFF7F7Fff, clusters[i][0] .. ", " .. clusters[i][1] .. " : " .. r .. "," .. g .. "," .. b);
 					end
 				else 
-					lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0x7F7F7Fff, possible_papy[i][0] .. ", " .. possible_papy[i][1] );
+					lsPrint(10, 80 + i*20, 0, 0.7, 0.7, 0x7F7F7Fff, clusters[i][0] .. ", " .. clusters[i][1] );
 				end
 			end
 		end
 		
-		lsSleep(100);
-		if clicked then
-			clickAll("UnPin.png", 1, 1);
-			--clickAll("This.png", 1, 1);
-			lsSleep(100);
-		end
+		lsSleep(50);
 	end
-
 end
